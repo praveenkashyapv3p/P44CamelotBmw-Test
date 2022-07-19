410a712b92c69d775d683fa057a6b0cb78481a0f
Praveen Kashyap <v3p.praveen@gmail.com>
2022-07-19 11:30:08 +0200

    Refactor code

--- src/main/java/com/camelot/p44camelotbmw/consumer/KafkaConsumerBMW.java
+++ src/main/java/com/camelot/p44camelotbmw/consumer/KafkaConsumerBMW.java
@@ -10,13 +10,12 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class KafkaConsumerBMW {
-    @Autowired
-    private RestTemplate restTemplate;
     BMWMapping bmwMapping = new BMWMapping();
     IdentifiersMapper identifiersMapping = new IdentifiersMapper();
     ContactInformationMapper contactInformationMapping = new ContactInformationMapper();
@@ -24,7 +23,10 @@ public class KafkaConsumerBMW {
     TransportLegMapper transportLegMapper = new TransportLegMapper();
     TechnicalDetailsMapper technicalDetailsMapper = new TechnicalDetailsMapper();
     DeliveryInformationMapper deliveryInformationMapper = new DeliveryInformationMapper();
-    //@KafkaListener(topics = "p44Data", groupId = "bmwGroup")
+    @Autowired
+    private RestTemplate restTemplate;
+    
+    @KafkaListener(topics = "p44Data", groupId = "bmwGroup")
     public void getTransformedMessage(String message) {
         Gson gson = new Gson();
         String jsonStartingString = "{\"records\":[{\"key\":";
@@ -32,7 +34,7 @@ public class KafkaConsumerBMW {
         String jsonEndString = "}]}";
         P44Shipment myObj = gson.fromJson(message, new TypeToken<P44Shipment>() {
         }.getType());
-        try{
+        try {
             identifiersMapping.mapIdentifiers(myObj, message, bmwMapping);
             contactInformationMapping.mapContactInfo(message, bmwMapping);
             currentLocationInfoMapper.mapCurrLocInfo(message, bmwMapping);
@@ -48,9 +50,9 @@ public class KafkaConsumerBMW {
             HttpEntity<String> entity = new HttpEntity<>(bmwJson, headers);
             ResponseEntity<String> response = restTemplate.postForEntity("https://p44-tracking-data-int.bmwgroup.com", entity, String.class);
             System.out.println("response: " + response.getBody());
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
+    
 }
--- src/main/java/com/camelot/p44camelotbmw/controller/SendEventController.java
+++ src/main/java/com/camelot/p44camelotbmw/controller/SendEventController.java
@@ -12,46 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class SendEventController {
     private final KafkaProducer producer;
     KafkaConsumerBMW kafkaConsumerBMW = new KafkaConsumerBMW();
-
+    
     public SendEventController(KafkaProducer producer) {
         this.producer = producer;
     }
-
+    
     @PostMapping(value = "/v1/sendEvent", consumes = "application/json")
     public ResponseEntity<String> shipmentDetailsFromP44(@RequestBody String shipmentJson) {
-//        Gson gson = new Gson();
-//        String jsonStartingString = "{\"records\":[{\"key\":";
-//        String jsonStringValue = ",\"value\":";
-//        String jsonEndString = "}]}";
-//        P44Shipment myObj = gson.fromJson(shipmentJson, new TypeToken<P44Shipment>() {
-//        }.getType());
-//        try{
-//            identifiersMapping.mapIdentifiers(myObj, shipmentJson, bmwMapping);
-//            contactInformationMapping.mapContactInfo(shipmentJson, bmwMapping);
-//            currentLocationInfoMapper.mapCurrLocInfo(shipmentJson, bmwMapping);
-//            bmwMapping.setTransportationNetwork("SHIP");
-//            bmwMapping.setMainTransportMode("SEA");
-//            transportLegMapper.mapTransportLegInfos(shipmentJson, bmwMapping);
-//            technicalDetailsMapper.mapTechnicalDetails(shipmentJson, bmwMapping);
-//            deliveryInformationMapper.mapDeliveryInformation(shipmentJson, bmwMapping);
-//            String jsonKey = String.valueOf(TechnicalDetailsMapper.get64MostSignificantBitsForVersion1());
-//            String bmwJson = jsonStartingString + jsonKey + jsonStringValue + new Gson().toJson(bmwMapping) + jsonEndString;
-//            this.producer.writeP44Message(jsonKey, bmwJson);
-//            return ResponseEntity.ok().build();//body(response.getBody());
-//        }catch (Exception e){
-//            return ResponseEntity.unprocessableEntity().build();
-//        }
         try {
             String jsonKey = String.valueOf(TechnicalDetailsMapper.get64MostSignificantBitsForVersion1());
-            //this.producer.writeP44Message(jsonKey, shipmentJson);
+            this.producer.writeP44Message(jsonKey, shipmentJson);
             kafkaConsumerBMW.getTransformedMessage(shipmentJson);
             return ResponseEntity.ok().build();
-        }catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity.internalServerError().build();
         }
     }
 }
-
-
-
-
