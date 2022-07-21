34b08b53a2af17f0d43796b1432cefd0c9394d72
Praveen Praveen <ppra@camelot-itlab.com>
2022-07-21 09:17:15 +0200

    Fix for Tracking Start JSON exception

--- src/main/java/com/camelot/p44camelotbmw/controller/SendEventController.java
+++ src/main/java/com/camelot/p44camelotbmw/controller/SendEventController.java
@@ -2,6 +2,10 @@ package com.camelot.p44camelotbmw.controller;
 
 import com.camelot.p44camelotbmw.jsonmapper.TechnicalDetailsMapper;
 import com.camelot.p44camelotbmw.producer.KafkaProducer;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,8 +23,18 @@ public class SendEventController {
     @PostMapping(value = "/v1/sendEvent", consumes = "application/json")
     public ResponseEntity<String> shipmentDetailsFromP44(@RequestBody String shipmentJson) {
         try {
+            boolean trackingJson = false;
             String jsonKey = String.valueOf(TechnicalDetailsMapper.get64MostSignificantBitsForVersion1());
-            this.producer.writeP44Message(jsonKey, shipmentJson);
+            JsonObject ShipJSON = (JsonObject) JsonParser.parseString(shipmentJson);
+            JsonArray events = (JsonArray) ShipJSON.get("events");
+            for (JsonElement eventsTyp : events) {
+                if (eventsTyp.getAsJsonObject().get("type").getAsString().equals("TRACKING_START")) {
+                    trackingJson = true;
+                }
+            }
+            if (!trackingJson) {
+                this.producer.writeP44Message(jsonKey, shipmentJson);
+            }
             return ResponseEntity.ok().build();
         } catch (Exception e) {
             return ResponseEntity.internalServerError().build();
