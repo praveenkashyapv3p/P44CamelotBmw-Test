95f80670abf079b28bef891990577c25e7fcd326
Praveen Kashyapa <v3p.praveen@gmail.com>
2022-08-29 16:23:39 +0200

    fix for carrierId

--- src/main/java/com/camelot/p44camelotbmw/p44JsonMapper/ContactInformationMapper.java
+++ src/main/java/com/camelot/p44camelotbmw/p44JsonMapper/ContactInformationMapper.java
@@ -6,12 +6,8 @@ import com.camelot.p44camelotbmw.entity.toBmwEntity.*;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import org.springframework.http.*;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 
@@ -28,63 +24,71 @@ public class ContactInformationMapper {
         List<ContactInformation> contactInformation1 = new ArrayList<>();
         //JsonObject relShipIdentJSON = (JsonObject) JsonParser.parseString(shipmentJson);
         String ShipIdent = shipmentJson.get("shipment").getAsJsonObject().get("id").getAsString();
-        String url = "https://na12.api.project44.com/api/v4/shipments/" + ShipIdent + "/tracking";
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBasicAuth("Camelot_Test@bmwsandbox.com", "c3hx>7U9^Y");
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        HttpEntity<String> request = new HttpEntity<>(headers);
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, request, String.class);
-        String jsonResponse = response.getBody();
-        JsonObject shipmentIdJSON = (JsonObject) JsonParser.parseString(jsonResponse);
+//        String url = "https://na12.api.project44.com/api/v4/shipments/" + ShipIdent + "/tracking";
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setBasicAuth("Camelot_Test@bmwsandbox.com", "c3hx>7U9^Y");
+//        headers.setContentType(MediaType.APPLICATION_JSON);
+//        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+//        HttpEntity<String> request = new HttpEntity<>(headers);
+//        RestTemplate restTemplate = new RestTemplate();
+//        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, request, String.class);
+//        String jsonResponse = response.getBody();
+//        JsonObject shipmentIdJSON = (JsonObject) JsonParser.parseString(jsonResponse);
     
         List<CreateShipment> shipmentIds = shipmentRepository.findByMasterShipmentId(ShipIdent);
         for (CreateShipment carrierIdFromDB : shipmentIds) {
-//            senderID = carrierIdFromDB.getSenderId();
-//            senderName = carrierIdFromDB.getSenderName();
-//            recipientID = carrierIdFromDB.getRecipientID();
-//            recipientName = carrierIdFromDB.getRecipientName();
+            senderID = carrierIdFromDB.getSenderId();
+            senderName = carrierIdFromDB.getSenderName();
+            recipientID = carrierIdFromDB.getRecipientID();
+            recipientName = carrierIdFromDB.getRecipientName();
             carrierID = carrierIdFromDB.getCarrierID();
             carrierName = carrierIdFromDB.getCarrierName();
         }
-        JsonArray attributes = (JsonArray) shipmentIdJSON.getAsJsonObject().get("attributes");
-        JsonArray identifiers = (JsonArray) shipmentIdJSON.getAsJsonObject().get("identifiers");
-        for (JsonElement id : identifiers) {
-            carrierIdFromGet = id.getAsJsonObject().get("type").getAsString();
-            if (carrierIdFromGet.equalsIgnoreCase("CARRIER_SCAC")) {
-                carrierIdFromGet = id.getAsJsonObject().get("value").getAsString();
-            }
-        }
-        for (JsonElement stp : attributes) {
-            senderID = stp.getAsJsonObject().get("name").getAsString();
-            if (senderID.equalsIgnoreCase("senderID")) {
-                JsonArray senderIdArray = (JsonArray) stp.getAsJsonObject().get("values");
-                senderID = senderIdArray.get(0).getAsString();
-            }
-            if (senderID.equalsIgnoreCase("senderName")) {
-                JsonArray senderIdArray = (JsonArray) stp.getAsJsonObject().get("values");
-                senderName = senderIdArray.get(0).getAsString();
-            }
-            if (senderID.equalsIgnoreCase("recipientID")) {
-                JsonArray senderIdArray = (JsonArray) stp.getAsJsonObject().get("values");
-                recipientID = senderIdArray.get(0).getAsString();
-            }
-            if (senderID.equalsIgnoreCase("recipientName")) {
-                JsonArray senderIdArray = (JsonArray) stp.getAsJsonObject().get("values");
-                recipientName = senderIdArray.get(0).getAsString();
-            }
-        }
+//        JsonArray attributes = (JsonArray) shipmentIdJSON.getAsJsonObject().get("attributes");
+//        JsonArray identifiers = (JsonArray) shipmentIdJSON.getAsJsonObject().get("identifiers");
+//        for (JsonElement id : identifiers) {
+//            carrierIdFromGet = id.getAsJsonObject().get("type").getAsString();
+//            if (carrierIdFromGet.equalsIgnoreCase("CARRIER_SCAC")) {
+//                carrierIdFromGet = id.getAsJsonObject().get("value").getAsString();
+//            }
+//        }
+//        for (JsonElement stp : attributes) {
+//            senderID = stp.getAsJsonObject().get("name").getAsString();
+//            if (senderID.equalsIgnoreCase("senderID")) {
+//                JsonArray senderIdArray = (JsonArray) stp.getAsJsonObject().get("values");
+//                senderID = senderIdArray.get(0).getAsString();
+//            }
+//            if (senderID.equalsIgnoreCase("senderName")) {
+//                JsonArray senderIdArray = (JsonArray) stp.getAsJsonObject().get("values");
+//                senderName = senderIdArray.get(0).getAsString();
+//            }
+//            if (senderID.equalsIgnoreCase("recipientID")) {
+//                JsonArray senderIdArray = (JsonArray) stp.getAsJsonObject().get("values");
+//                recipientID = senderIdArray.get(0).getAsString();
+//            }
+//            if (senderID.equalsIgnoreCase("recipientName")) {
+//                JsonArray senderIdArray = (JsonArray) stp.getAsJsonObject().get("values");
+//                recipientName = senderIdArray.get(0).getAsString();
+//            }
+//        }
     
         sender.setSenderID(senderID);
         sender.setName(senderName);
         recepient.setRecipientID(recipientID);
         recepient.setName(recipientName);
         if (carrierID.equalsIgnoreCase("")) {
-            carrier.setCarrierID(carrierIdFromGet);
-        } else {
-            carrier.setCarrierID(carrierID);
+            JsonArray stops = (JsonArray) shipmentJson.get("shipment").getAsJsonObject().get("identifiers");
+            for (JsonElement position : stops) {
+                if ("CARRIER_SCAC".equals(position.getAsJsonObject().get("type").getAsString())) {
+                    carrierID = position.getAsJsonObject().get("value").getAsString();
+                    carrier.setCarrierID(carrierID);
+                }
+            }
+        
         }
+//        else {
+//            carrier.setCarrierID(carrierID);
+//        }
         carrier.setCarrierName(carrierName);
     
         contactInformation.setCarrier(carrier);
