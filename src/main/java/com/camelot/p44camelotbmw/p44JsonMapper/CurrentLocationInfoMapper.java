2410fac7a3be71868112dbe7664f23f6e7da837c
Praveen Kashyapa <v3p.praveen@gmail.com>
2022-09-01 15:11:35 +0200

    fix for missing positions field

--- src/main/java/com/camelot/p44camelotbmw/p44JsonMapper/CurrentLocationInfoMapper.java
+++ src/main/java/com/camelot/p44camelotbmw/p44JsonMapper/CurrentLocationInfoMapper.java
@@ -20,7 +20,7 @@ public class CurrentLocationInfoMapper {
         //JsonObject ShipJSON = (JsonObject) JsonParser.parseString(shipmentJson);
         JsonArray stops = (JsonArray) shipmentJson.get("shipment").getAsJsonObject().get("routeInfo").getAsJsonObject().get("stops");
         JsonArray events = (JsonArray) shipmentJson.get("events");
-        JsonArray positions = (JsonArray) shipmentJson.get("positions");
+    
         JsonArray states = (JsonArray) shipmentJson.get("states");
         for (JsonElement eventsTyp : events) {
             if (eventsTyp.getAsJsonObject().has("dateTime")) {
@@ -29,14 +29,15 @@ public class CurrentLocationInfoMapper {
                 eventsType = eventsTyp.getAsJsonObject().get("type").getAsString();
             }
         }
-    
-        if (positions.size() > 0) {
-            JsonElement posArray = positions.get(positions.size() - 1);
-            longitude = posArray.getAsJsonObject().get("longitude").getAsString();
-            latitude = posArray.getAsJsonObject().get("latitude").getAsString();
-            geoDateTimeUTC = posArray.getAsJsonObject().get("dateTime").getAsString();
+        if (shipmentJson.has("positions")) {
+            JsonArray positions = (JsonArray) shipmentJson.get("positions");
+            if (positions.size() > 0) {
+                JsonElement posArray = positions.get(positions.size() - 1);
+                longitude = posArray.getAsJsonObject().get("longitude").getAsString();
+                latitude = posArray.getAsJsonObject().get("latitude").getAsString();
+                geoDateTimeUTC = posArray.getAsJsonObject().get("dateTime").getAsString();
+            }
         }
-    
     
         for (JsonElement position : stops) {
             assert eventStopId != null;
@@ -60,11 +61,9 @@ public class CurrentLocationInfoMapper {
                 }
                 if (statusName.equalsIgnoreCase("AT_STOP")) {
                     JsonElement relShipIdentifiers = position.getAsJsonObject().get("location").getAsJsonObject().get("coordinates");
-                    //for (JsonElement relShipIdent : relShipIdentifiers) {
                     latitude = relShipIdentifiers.getAsJsonObject().get("latitude").getAsString();
                     longitude = relShipIdentifiers.getAsJsonObject().get("longitude").getAsString();
                     geoDateTimeUTC = timeStamp;
-                    //}
                 }
                 currentLocationInfos.setGeoDateTimeUTC(geoDateTimeUTC);
                 if (!longitude.equals("") && !latitude.equals("")) {
